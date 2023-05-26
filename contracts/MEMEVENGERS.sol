b2635a47423c32bb98f5b9c398e5e49108b871af
memevengers <memevengers1963@gmail.com>
2023-05-26 15:51:45 +0900

    Intial commit

---
+++ contracts/MEMEVENGERS.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MemevengersToken is ERC20, ERC20Burnable, Ownable {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 initialSupply
+    ) ERC20(name_, symbol_) {
+        _mint(msg.sender, initialSupply);
+    }
+}
